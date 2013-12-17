b5280509bac937e8ebee71e444473482ceb25c1f
chanble <chanbo.cn@gmail.com>
2013-12-17 10:56:30 +0800

    change chinese char to unicode

--- src/jquery.realeditor.js
+++ src/jquery.realeditor.js
@@ -105,11 +105,11 @@
 					, fontList = '', elOffset = jel.offset();
 				var ulLeft = elOffset.left
 					,ulTop = elOffset.top + jel.innerHeight();
-				var fonts = [{"key":"SimSun", "title":"宋体","label":"宋体"}
-							,{"key":"FangSong_GB2312", "title":"仿宋","label":"仿宋"}
-							,{"key":"SimHei", "title":"黑体","label":"黑体"}
-							,{"key":"KaiTi_GB2312", "title":"楷体","label":"楷体"}
-							,{"key":"Microsoft YaHei", "title":"微软雅黑","label":"微软雅黑"}
+				var fonts = [{"key":"SimSun", "title":"\u5b8b\u4f53","label":"\u5b8b\u4f53"}//宋体
+							,{"key":"FangSong_GB2312", "title":"\u4eff\u5b8b","label":"\u4eff\u5b8b"}//仿宋
+							,{"key":"SimHei", "title":"\u9ed1\u4f53","label":"\u9ed1\u4f53"}//黑体
+							,{"key":"KaiTi_GB2312", "title":"\u6977\u4f53","label":"\u6977\u4f53"}//楷体
+							,{"key":"Microsoft YaHei", "title":"\u5fae\u8f6f\u96c5\u9ed1","label":"\u5fae\u8f6f\u96c5\u9ed1"}//微软雅黑
 							,{"key":"Arial", "title":"Arial","label":"Arial"}
 							,{"key":"Arial Black", "title":"Arial Black","label":"Arial Black"}
 							,{"key":"Sans", "title":"Sans","label":"Sans"}
@@ -162,13 +162,13 @@
 					, fontList = '', elOffset = jel.offset();
 				var ulLeft = elOffset.left
 					,ulTop = elOffset.top + jel.innerHeight();
-				var fonts = [{"key":"1", "title":"极小","label":"极小"}
-							,{"key":"2", "title":"特小","label":"特小"}
-							,{"key":"3", "title":"小","label":"小"}
-							,{"key":"4", "title":"中等","label":"中等"}
-							,{"key":"5", "title":"大","label":"大"}
-							,{"key":"6", "title":"特大","label":"特大"}
-							,{"key":"7", "title":"极大","label":"极大"}];
+				var fonts = [{"key":"1", "title":"\u6781\u5c0f","label":"\u6781\u5c0f"}//极小
+							,{"key":"2", "title":"\u7279\u5c0f","label":"\u7279\u5c0f"}//特小
+							,{"key":"3", "title":"\u5c0f","label":"\u5c0f"}//小
+							,{"key":"4", "title":"\u4e2d\u7b49","label":"\u4e2d\u7b49"}//中等
+							,{"key":"5", "title":"\u5927","label":"\u5927"}//大
+							,{"key":"6", "title":"\u7279\u5927","label":"\u7279\u5927"}//特大
+							,{"key":"7", "title":"\u6781\u5927","label":"\u6781\u5927"}];//极大
 				for(var i in fonts){
 					fontList += '<li title="'+fonts[i].title
 							+'"><a><font size="'
@@ -209,107 +209,107 @@
 					, fontList = '', elOffset = jel.offset();
 				var ulLeft = elOffset.left
 					,ulTop = elOffset.top + jel.innerHeight();
-				var colors = [{"key":"#FFFFFF", "label":"White","title":"白色"}
-						,{"key":"#D3D3D3","label":"LightGray","title":"浅灰色"}
-						,{"key":"#808080","label":"Gray","title":"灰色"}
-						//,{"key":"#A9A9A9","label":"DarkGray","title":"深灰色"}
-						,{"key":"#696969","label":"DimGray","title":"暗灰色"}
-						,{"key":"#000000","label":"Black","title":"黑色"}
-						//,{"key":"#FFFFF0","label":"Ivory","title":"象牙白"}
-						//,{"key":"#FFFFE0","label":"LightYellow","title":"浅黄色"}
-						,{"key":"#FFFF00","label":"Yellow","title":"黄色"}
-						//,{"key":"#FFFAFA","label":"Snow","title":"雪白色"}
-						//,{"key":"#FFFAF0","label":"FloralWhite","title":"花白色"}
-						//,{"key":"#FFFACD","label":"LemonChiffon","title":"粉黄色"}
-						//,{"key":"#FFF8DC","label":"Cornsilk","title":"米绸色"}
-						//,{"key":"#FFF5EE","label":"SeaShell","title":"海贝色"}
-						,{"key":"#FFF0F5","label":"LavenderBlush","title":"淡紫红"}
-						//,{"key":"#FFEFD5","label":"PapayaWhip","title":"番木色"}
-						//,{"key":"#FFEBCD","label":"BlanchedAlmond","title":"白杏色"}
-						//,{"key":"#FFE4E1","label":"MistyRose","title":"浅玫瑰色"}
-						,{"key":"#FFE4C4","label":"Bisque","title":"黄褐色"}
+				var colors = [{"key":"#FFFFFF", "label":"White","title":"\u767d\u8272"}//白色
+						,{"key":"#D3D3D3","label":"LightGray","title":"\u6d45\u7070\u8272"}//浅灰色
+						,{"key":"#808080","label":"Gray","title":"\u7070\u8272"}//灰色
+						//,{"key":"#A9A9A9","label":"DarkGray","title":"深灰色"}//深灰色
+						,{"key":"#696969","label":"DimGray","title":"\u6697\u7070\u8272"}//暗灰色
+						,{"key":"#000000","label":"Black","title":"\u9ed1\u8272"}//黑色
+						//,{"key":"#FFFFF0","label":"Ivory","title":"象牙白"}//象牙白
+						//,{"key":"#FFFFE0","label":"LightYellow","title":"浅黄色"}//浅黄色
+						,{"key":"#FFFF00","label":"Yellow","title":"\u9ec4\u8272"}//黄色
+						//,{"key":"#FFFAFA","label":"Snow","title":"雪白\u8272"}//雪白色
+						//,{"key":"#FFFAF0","label":"FloralWhite","title":"花白\u8272"}//花白色
+						//,{"key":"#FFFACD","label":"LemonChiffon","title":"粉黄\u8272"}
+						//,{"key":"#FFF8DC","label":"Cornsilk","title":"米绸\u8272"}
+						//,{"key":"#FFF5EE","label":"SeaShell","title":"海贝\u8272"}
+						,{"key":"#FFF0F5","label":"LavenderBlush","title":"\u6de1\u7d2b\u8272"}
+						//,{"key":"#FFEFD5","label":"PapayaWhip","title":"番木\u8272"}
+						//,{"key":"#FFEBCD","label":"BlanchedAlmond","title":"白杏\u8272"}
+						//,{"key":"#FFE4E1","label":"MistyRose","title":"浅玫瑰\u8272"}
+						,{"key":"#FFE4C4","label":"Bisque","title":"\u9ec4\u8910\u8272"}
 						//,{"key":"#FFE4B5","label":"Moccasin","title":"莫卡辛"}
 						//,{"key":"#FFDEAD","label":"NavajoWhite","title":"土著白"}
-						//,{"key":"#FFDAB9","label":"PeachPuff","title":"桃红色"}
-						,{"key":"#FFD700","label":"Gold","title":"金黄色"}
-						,{"key":"#FFC0CB","label":"Pink","title":"粉红色"}
+						//,{"key":"#FFDAB9","label":"PeachPuff","title":"桃红\u8272"}
+						,{"key":"#FFD700","label":"Gold","title":"\u91d1\u9ec4\u8272"}
+						,{"key":"#FFC0CB","label":"Pink","title":"\u7c89\u7ea2\u8272"}
 						//,{"key":"#FFB6C1","label":"LightPink","title":"浅粉红"}
-						,{"key":"#FFA500","label":"Orange","title":"橙色"}
+						,{"key":"#FFA500","label":"Orange","title":"\u6a59\u8272"}
 						//,{"key":"#FFA07A","label":"LightSalmon","title":"浅肉色"}
-						,{"key":"#FF8C00","label":"DarkOrange","title":"深橙色"}
-						,{"key":"#FF7F50","label":"Coral","title":"珊瑚红"}
+						,{"key":"#FF8C00","label":"DarkOrange","title":"\u6df1\u6a59\u8272"}
+						,{"key":"#FF7F50","label":"Coral","title":"\u73ca\u745a\u8272"}
 						//,{"key":"#FF69B4","label":"HotPink","title":"艳粉色"}
 						//,{"key":"#FF6347","label":"Tomato","title":"番茄红"}
-						,{"key":"#FF4500","label":"OrangeRed","title":"橘红色"}
-						,{"key":"#FF1493","label":"DeepPink","title":"深粉色"}
+						,{"key":"#FF4500","label":"OrangeRed","title":"\u6a58\u7ea2\u8272"}
+						,{"key":"#FF1493","label":"DeepPink","title":"\u6df1\u7c89\u8272"}
 						//,{"key":"#FF00FF","label":"Fuchsia","title":"紫红色"}
 						//,{"key":"#FF00FF","label":"Magenta","title":"洋红色"}
-						,{"key":"#FF0000","label":"Red","title":"红色"}
+						,{"key":"#FF0000","label":"Red","title":"\u7ea2\u8272"}
 						//,{"key":"#FDF5E6","label":"OldLace","title":"浅米色"}
 						//,{"key":"#FAFAD2","label":"LightGoldenRodYellow","title":"浅秋黄"}
 						//,{"key":"#FAF0E6","label":"Linen","title":"亚麻色"}
 						//,{"key":"#FAEBD7","label":"AntiqueWhite","title":"古董白"}
-						,{"key":"#FA8072","label":"Salmon","title":"粉橙色"}
+						,{"key":"#FA8072","label":"Salmon","title":"\u7c89\u6a59\u8272"}
 						//,{"key":"#F8F8FF","label":"GhostWhite","title":"幽灵白"}
-						,{"key":"#F5FFFA","label":"MintCream","title":"薄荷乳"}
-						,{"key":"#F5F5F5","label":"WhiteSmoke","title":"烟白色"}
+						,{"key":"#F5FFFA","label":"MintCream","title":"\u8584\u8377\u4e73"}
+						,{"key":"#F5F5F5","label":"WhiteSmoke","title":"\u70df\u767d\u8272"}
 						//,{"key":"#F5F5DC","label":"Beige","title":"灰褐色"}
-						,{"key":"#F5DEB3","label":"Wheat","title":"淡褐色"}
+						,{"key":"#F5DEB3","label":"Wheat","title":"\u6de1\u8910\u8272"}
 						//,{"key":"#F4A460","label":"SandyBrown","title":"沙褐色"}
-						,{"key":"#F0FFFF","label":"Azure","title":"天蓝色"}
+						,{"key":"#F0FFFF","label":"Azure","title":"\u5929\u84dd\u8272"}
 						//,{"key":"#F0FFF0","label":"HoneyDew","title":"哈密瓜蓝"}
-						,{"key":"#F0F8FF","label":"AliceBlue","title":"浅蓝色"}
-						,{"key":"#F0E68C","label":"Khaki","title":"卡其色"}
+						,{"key":"#F0F8FF","label":"AliceBlue","title":"\u6d45\u84dd\u8272"}
+						,{"key":"#F0E68C","label":"Khaki","title":"\u5361\u5176\u8272"}
 						//,{"key":"#F08080","label":"LightCoral","title":"淡珊瑚"}
 						//,{"key":"#EEE8AA","label":"PaleGoldenRod","title":"灰秋黄"}
-						,{"key":"#EE82EE","label":"Violet","title":"蓝紫色"}
+						,{"key":"#EE82EE","label":"Violet","title":"\u84dd\u7d2b\u8272"}
 						//,{"key":"#E9967A","label":"DarkSalmon","title":"暗肉色"}
-						,{"key":"#E6E6FA","label":"Lavender","title":"淡紫色"}
+						,{"key":"#E6E6FA","label":"Lavender","title":"\u6de1\u7d2b\u8272"}
 						//,{"key":"#E0FFFF","label":"LightCyan","title":"淡青色"}
 						//,{"key":"#DEB887","label":"BurlyWood","title":"实木色"}
-						,{"key":"#DDA0DD","label":"Plum","title":"绛紫色"}
+						,{"key":"#DDA0DD","label":"Plum","title":"\u7edb\u7d2b\u8272"}
 						//,{"key":"#DCDCDC","label":"Gainsboro","title":"淡灰色"}
-						,{"key":"#DC143C","label":"Crimson","title":"深红色"}
+						,{"key":"#DC143C","label":"Crimson","title":"\u6df1\u7ea2\u8272"}
 						//,{"key":"#DB7093","label":"PaleVioletRed","title":"淡紫红"}
 						//,{"key":"#DAA520","label":"GoldenRod","title":"褐黄色"}
 						//,{"key":"#DA70D6","label":"Orchid","title":"兰花紫"}
 						//,{"key":"#D8BFD8","label":"Thistle","title":"蓟色"}
-						,{"key":"#D2B48C","label":"Tan","title":"棕褐色"}
-						,{"key":"#D2691E","label":"Chocolate","title":"巧克力色"}
+						,{"key":"#D2B48C","label":"Tan","title":"\u68d5\u8910\u8272"}
+						,{"key":"#D2691E","label":"Chocolate","title":"\u5de7\u514b\u529b\u8272"}
 						//,{"key":"#CD853F","label":"Peru","title":"秘鲁色"}
-						,{"key":"#CD5C5C","label":"IndianRed","title":"印度红"}
+						,{"key":"#CD5C5C","label":"IndianRed","title":"\u5370\u5ea6\u8272"}
 						//,{"key":"#C71585","label":"MediumVioletRed","title":"紫罗兰红"}
 						//,{"key":"#C0C0C0","label":"Silver","title":"银白色"}
 						//,{"key":"#BDB76B","label":"DarkKhaki","title":"深卡其布"}
-						,{"key":"#BC8F8F","label":"RosyBrown","title":"玫瑰棕"}
+						,{"key":"#BC8F8F","label":"RosyBrown","title":"\u73ab\u7470\u8272"}
 						//,{"key":"#BA55D3","label":"MediumOrchid","title":"中兰花紫"}
 						//,{"key":"#B8860B","label":"DarkGoldenRod","title":"深秋色"}
-						,{"key":"#B22222","label":"FireBrick","title":"砖红色"}
+						,{"key":"#B22222","label":"FireBrick","title":"\u7816\u7ea2\u8272"}
 						//,{"key":"#B0E0E6","label":"PowderBlue","title":"深蓝色"}
 						//,{"key":"#B0C4DE","label":"LightSteelBlue","title":"淡钢蓝"}
 						//,{"key":"#AFEEEE","label":"PaleTurquoise","title":"淡宝石绿"}
 						//,{"key":"#ADFF2F","label":"GreenYellow","title":"绿黄色"}
-						,{"key":"#ADD8E6","label":"LightBlue","title":"浅蓝色"}
-						,{"key":"#A52A2A","label":"Brown","title":"棕色"}
+						,{"key":"#ADD8E6","label":"LightBlue","title":"\u6d45\u84dd\u8272"}
+						,{"key":"#A52A2A","label":"Brown","title":"\u68d5\u8272"}
 						//,{"key":"#A0522D","label":"Sienna","title":"土黄色"}
-						,{"key":"#9ACD32","label":"YellowGreen","title":"黄绿色"}
+						,{"key":"#9ACD32","label":"YellowGreen","title":"\u9ec4\u7eff\u8272"}
 						//,{"key":"#9932CC","label":"DarkOrchid","title":"暗紫色"}
 						//,{"key":"#98FB98","label":"PaleGreen","title":"淡绿色"}
-						,{"key":"#9400D3","label":"DarkViolet","title":"暗紫色"}
+						,{"key":"#9400D3","label":"DarkViolet","title":"\u6697\u7d2b\u8272"}
 						//,{"key":"#9370DB","label":"MediumPurple","title":"中紫色"}
-						,{"key":"#90EE90","label":"LightGreen","title":"浅绿色"}
-						,{"key":"#8FBC8F","label":"DarkSeaGreen","title":"深海绿"}
+						,{"key":"#90EE90","label":"LightGreen","title":"\u6d45\u7eff\u8272"}
+						,{"key":"#8FBC8F","label":"DarkSeaGreen","title":"\u6df1\u6d77\u7eff"}
 						//,{"key":"#8B4513","label":"SaddleBrown","title":"重褐色"}
 						//,{"key":"#8B008B","label":"DarkMagenta","title":"深洋红"}
-						,{"key":"#8B0000","label":"DarkRed","title":"深红色"}
-						,{"key":"#8A2BE2","label":"BlueViolet","title":"蓝紫色"}
+						,{"key":"#8B0000","label":"DarkRed","title":"\u6df1\u7ea2\u8272"}
+						,{"key":"#8A2BE2","label":"BlueViolet","title":"\u84dd\u7d2b\u8272"}
 						//,{"key":"#87CEFA","label":"LightSkyBlue","title":"浅天蓝色"}
-						,{"key":"#87CEEB","label":"SkyBlue","title":"天蓝色"}
-						,{"key":"#808000","label":"Olive","title":"橄榄色"}
-						,{"key":"#800080","label":"Purple","title":"紫色的"}
-						,{"key":"#800000","label":"Maroon","title":"褐红色"}
+						,{"key":"#87CEEB","label":"SkyBlue","title":"\u5929\u84dd\u8272"}
+						,{"key":"#808000","label":"Olive","title":"\u6a44\u6984\u8272"}
+						,{"key":"#800080","label":"Purple","title":"\u7d2b\u8272"}
+						,{"key":"#800000","label":"Maroon","title":"\u8910\u7ea2\u8272"}
 						//,{"key":"#7FFFD4","label":"Aquamarine","title":"宝石蓝"}
-						,{"key":"#7FFF00","label":"Chartreuse","title":"浅绿色"}
+						,{"key":"#7FFF00","label":"Chartreuse","title":"\u6d45\u7eff\u8272"}
 						//,{"key":"#7CFC00","label":"LawnGreen","title":"草绿色"}
 						//,{"key":"#7B68EE","label":"MediumSlateBlue","title":"中板岩蓝"}
 						//,{"key":"#778899","label":"LightSlateGray","title":"浅石板灰"}
@@ -317,37 +317,37 @@
 						//,{"key":"#6B8E23","label":"OliveDrab","title":"橄榄褐"}
 						//,{"key":"#6A5ACD","label":"SlateBlue","title":"岩蓝色"}
 						//,{"key":"#66CDAA","label":"MediumAquaMarine","title":"中碧绿色"}
-						,{"key":"#6495ED","label":"CornflowerBlue","title":"浅蓝的"}
-						,{"key":"#5F9EA0","label":"CadetBlue","title":"军蓝色"}
+						,{"key":"#6495ED","label":"CornflowerBlue","title":"\u6d45\u84dd\u8272"}
+						,{"key":"#5F9EA0","label":"CadetBlue","title":"\u519b\u84dd\u8272"}
 						//,{"key":"#556B2F","label":"DarkOliveGreen","title":"暗橄榄绿"}
-						,{"key":"#4B0082","label":"Indigo","title":"靛蓝色"}
+						,{"key":"#4B0082","label":"Indigo","title":"\u975b\u84dd\u8272"}
 						//,{"key":"#48D1CC","label":"MediumTurquoise","title":"宝石绿"}
 						//,{"key":"#483D8B","label":"DarkSlateBlue","title":"暗灰蓝色"}
-						,{"key":"#4682B4","label":"SteelBlue","title":"钢青色"}
-						,{"key":"#4169E1","label":"RoyalBlue","title":"品蓝色"}
-						,{"key":"#40E0D0","label":"Turquoise","title":"青绿色"}
+						,{"key":"#4682B4","label":"SteelBlue","title":"\u94a2\u9752\u8272"}
+						,{"key":"#4169E1","label":"RoyalBlue","title":"\u54c1\u84dd\u8272"}
+						,{"key":"#40E0D0","label":"Turquoise","title":"\u9752\u7eff\u8272"}
 						//,{"key":"#3CB371","label":"MediumSeaGreen","title":"中海绿色"}
 						//,{"key":"#32CD32","label":"LimeGreen","title":"石灰绿"}
-						,{"key":"#2F4F4F","label":"DarkSlateGray","title":"墨绿色"}
-						,{"key":"#2E8B57","label":"SeaGreen","title":"海绿色"}
-						,{"key":"#228B22","label":"ForestGreen","title":"葱绿色"}
+						,{"key":"#2F4F4F","label":"DarkSlateGray","title":"\u58a8\u7eff\u8272"}
+						,{"key":"#2E8B57","label":"SeaGreen","title":"\u6d77\u7eff\u8272"}
+						,{"key":"#228B22","label":"ForestGreen","title":"\u8471\u7eff\u8272"}
 						//,{"key":"#20B2AA","label":"LightSeaGreen","title":"浅海绿"}
-						,{"key":"#1E90FF","label":"DodgerBlue","title":"闪蓝色"}
+						,{"key":"#1E90FF","label":"DodgerBlue","title":"\u95ea\u84dd\u8272"}
 						//,{"key":"#191970","label":"MidnightBlue","title":"深兰色"}
 						//,{"key":"#00FFFF","label":"Aqua","title":"湖绿色"}
-						,{"key":"#00FFFF","label":"Cyan","title":"青色"}
+						,{"key":"#00FFFF","label":"Cyan","title":"\u9752\u8272"}
 						//,{"key":"#00FF7F","label":"SpringGreen","title":"春绿色"}
-						,{"key":"#00FF00","label":"Lime","title":"柠檬色"}
+						,{"key":"#00FF00","label":"Lime","title":"\u67e0\u6aac\u8272"}
 						//,{"key":"#00FA9A","label":"MediumSpringGreen","title":"中春绿色"}
 						//,{"key":"#00CED1","label":"DarkTurquoise","title":"深宝石绿"}
 						//,{"key":"#00BFFF","label":"DeepSkyBlue","title":"深天蓝色"}
 						//,{"key":"#008B8B","label":"DarkCyan","title":"深青色"}
-						,{"key":"#008080","label":"Teal","title":"蓝绿色"}
-						,{"key":"#008000","label":"Green","title":"绿色"}
+						,{"key":"#008080","label":"Teal","title":"\u84dd\u7eff\u8272"}
+						,{"key":"#008000","label":"Green","title":"\u7eff\u8272"}
 						//,{"key":"#006400","label":"DarkGreen","title":"深绿色"}
-						,{"key":"#0000FF","label":"Blue","title":"蓝色"}
+						,{"key":"#0000FF","label":"Blue","title":"\u84dd\u8272"}
 						//,{"key":"#0000CD","label":"MediumBlue","title":"中蓝色"}
-						,{"key":"#00008B","label":"DarkBlue","title":"深蓝色"}
+						,{"key":"#00008B","label":"DarkBlue","title":"\u6df1\u84dd\u8272"}
 						//,{"key":"#000080","label":"Navy","title":"海军蓝"}
 						];
 				fontList += '<div>';
@@ -356,7 +356,7 @@
 					if (ti%9 === 0){
 						fontList += '</div><div>';
 					}else{
-						fontList += '<a href="#" title="'+colors[i].title
+						fontList += '<a href="#" onclick="return false;" title="'+colors[i].title
 								+'"style="display: inline-block;border: #999 1px solid;width: 17px;margin: 1px;height: 12px; background-color:'
 								+colors[i].key+';"></a>';
 					}
@@ -554,7 +554,7 @@
 			var label = !!ot.label ? ot.label : '';
 			var shortcutKey = !!ot.shortcutKey ? ot.shortcutKey : 0;
 			var shortcutKeyStr = !!shortcutKey ? '('+shortcutKey+')' : '';
-			var toolStr = '<span><a href="#" class="rltoolbutton" id="rltoolabutton'
+			var toolStr = '<span><a href="#" onclick="return false;" class="rltoolbutton" id="rltoolabutton'
 							+key+'" title="'+label + shortcutKeyStr
 							+'"><span class="rltoolicon rltoolicon-'
 							+key+'">'
